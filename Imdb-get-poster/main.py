cb348682198fe0d728426ec0404dd705ef88256f
Aditya Narayan Shahi <adityan.shahi2014@gmail.com>
2018-01-26 23:41:40 +0530

    Created Imdb-get-poster

---
+++ Imdb-get-poster/main.py
@@ -0,0 +1,26 @@
+import requests,bs4
+name=input("Enter movie/Tv series name\n");
+print("Wait...")
+try:
+    googlelink="https://www.google.com/search?q=imdb "+name
+    #print(googlelink)
+    res=requests.get(googlelink);
+    soup=bs4.BeautifulSoup(res.text,'html.parser')
+    #print(soup)
+    div = soup.find("div", {"class": "kv"})
+    cite = div.find("cite")
+    link="https://www.imdb"+cite.contents[2];
+    #print(link)
+    res=requests.get(link)
+    soup=bs4.BeautifulSoup(res.text,'html.parser')
+    div=soup.find("div",{"class":"poster"})
+    image=div.find("img",{"itemprop":"image"})
+    ilink=image['src']
+    iname=name+".jpg"
+    f=open(iname,'wb')
+    f.write(requests.get(ilink).content)
+    f.close()
+    print("DONE. Image saved as "+iname)
+except:
+    print("There was some error. Check name or internet connection")
+
