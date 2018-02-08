4dcc37dc8a46707951fe46d9285a3344b22b841a
Aditya Narayan Shahi <adityan.shahi2014@gmail.com>
2018-02-08 13:42:22 +0530

    Made changes to Local weather

--- Local-Weather/main.py
+++ Local-Weather/main.py
@@ -1,18 +1,21 @@
 import requests,bs4
-name=input("Enter location\n");
-print("Wait...")
-try:
-    res=requests.get("http://maps.google.com/maps/api/geocode/json",params={'address':name})
-    lat=res.json()['results'][0]['geometry']['location']['lat']
-    long=res.json()['results'][0]['geometry']['location']['lng']
-    link="https://api.darksky.net/forecast/94f3df5dac93c73774df3d273af106fa/"+str(lat)+","+str(long)
-    res=requests.get(link)
-    print("Temperature: "+str(res.json()['currently']['temperature']))
-    print("Humidity: "+str(res.json()['currently']['humidity']))
-    print("Wind Speed: "+str(res.json()['currently']['windSpeed']))
-    print("Current Forecast: "+str(res.json()['currently']['summary']))
-    print("Today's Forecast: "+str(res.json()['hourly']['summary']))
-    print("Week's Forecast: "+str(res.json()['hourly']['summary']))
-except:
-    print("There was some error. Check name or internet connection")
-input("\n\nPress enter key to quit")
+while True:
+    name=input("Enter location or type in QUIT to quit\n");
+    if(name=="QUIT"):
+        break
+    print("Wait...")
+    try:
+        res=requests.get("http://maps.google.com/maps/api/geocode/json",params={'address':name})
+        lat=res.json()['results'][0]['geometry']['location']['lat']
+        long=res.json()['results'][0]['geometry']['location']['lng']
+        link="https://api.darksky.net/forecast/94f3df5dac93c73774df3d273af106fa/"+str(lat)+","+str(long)
+        res=requests.get(link)
+        print("Temperature: "+str(res.json()['currently']['temperature']))
+        print("Humidity: "+str(res.json()['currently']['humidity']))
+        print("Wind Speed: "+str(res.json()['currently']['windSpeed']))
+        print("Current Forecast: "+str(res.json()['currently']['summary']))
+        print("Today's Forecast: "+str(res.json()['hourly']['summary']))
+        print("Week's Forecast: "+str(res.json()['hourly']['summary']))
+        print("\n")
+    except:
+        print("There was some error. Check name or internet connection\n")
