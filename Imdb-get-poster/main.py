d19a6f80f3e550acbbcc7ff75aff2145a35a8264
Aditya Narayan Shahi <adityan.shahi2014@gmail.com>
2018-08-03 14:36:04 +0530

    Updated

--- Imdb-get-poster/main.py
+++ Imdb-get-poster/main.py
@@ -7,10 +7,13 @@ try:
     res=requests.get(googlelink);
     soup=bs4.BeautifulSoup(res.text,'html.parser')
     #print(soup)
-    div = soup.find("div", {"class": "kv"})
+    div = soup.find("div", {"class": "hJND5c"})
     cite = div.find("cite")
+    unknown="episodes"
     link="https://www.imdb"+cite.contents[2];
-    #print(link)
+    '''if(link.endswith(unkown)):
+        link=link[:]'''
+    print(link)
     res=requests.get(link)
     soup=bs4.BeautifulSoup(res.text,'html.parser')
     div=soup.find("div",{"class":"poster"})
